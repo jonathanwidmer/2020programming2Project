321d221baa5d09a0e89dc00b8e31f51eaa6839bf
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-08 10:14:45 -0700

    Update Static.java

--- src/Static.java
+++ src/Static.java
@@ -1,7 +1,27 @@
 public class Static{
   private int health;
+  /**
+  @param player choice
+  allows player to choose character
+  @return player name
+  */
   private string player;
+  /**
+  @param car choice
+  allows player to choose car
+  @return car name
+  */
   private string car;
+  /**
+  @param weapon choice
+  allows player to choose weapon
+  @return weapon name
+  */
   private string weapon;
+  /**
+  @param course choice
+  allows player to choose course
+  @return course name
+  */
   private static int course;
 }
