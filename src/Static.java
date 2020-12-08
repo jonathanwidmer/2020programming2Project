29a739734de6e11bfa0c0b042b1e9a4ea6966726
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-08 10:16:05 -0700

    Update Static.java

--- src/Static.java
+++ src/Static.java
@@ -3,24 +3,28 @@ public class Static{
   /**
   @param player choice
   allows player to choose character
+  TODO - add characters
   @return player name
   */
   private string player;
   /**
   @param car choice
   allows player to choose car
+  TODO - add cars
   @return car name
   */
   private string car;
   /**
   @param weapon choice
   allows player to choose weapon
+  TODO - add weapons
   @return weapon name
   */
   private string weapon;
   /**
   @param course choice
   allows player to choose course
+  TODO - add courses
   @return course name
   */
   private static int course;
