ac710d2682cd3ae348b9177739b3d9e705fd9ebf
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-08 10:15:21 -0700

    Update States.java

--- src/States.java
+++ src/States.java
@@ -4,6 +4,7 @@ public class States{
   public static final int WEAK = 50;
   public static final int ALIVE = 100;
   /**
+  TODO - make if statements for current health
   describes your current state of health
   @return your health and current state
   */
