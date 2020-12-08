df3c2f19f62a88768271c6a2a332428c8adcf925
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-08 10:09:00 -0700

    Update States.java

--- src/States.java
+++ src/States.java
@@ -3,4 +3,8 @@ public class States{
   public static final int DYING = 20;
   public static final int WEAK = 50;
   public static final int ALIVE = 100;
+  /**
+  describes your current state of health
+  @return your health and current state
+  */
 }
