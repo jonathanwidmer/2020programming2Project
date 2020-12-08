a1e245232cc396dc0e44a88490a743e74b934718
jonathanwidmer <61252576+jonathanwidmer@users.noreply.github.com>
2020-12-08 10:07:18 -0700

    Update Moving.java

--- src/Moving.java
+++ src/Moving.java
@@ -2,6 +2,10 @@ public class Moving{
   private int row;
   private int column;
   private int direction;
+  /**
+  TODO - make directions North, South, East, West
+  @param a key pressed on keyboard to make object move certain direction
+  */
   public void move{
     if(direction == NORTH){
       row++;
